8faffedd2eb3290bc6468be9cabfcc209718e448
tatsunori.kat@spumail.net <norijang.kato@gmail.com>
2024-11-16 23:47:13 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+import './App.css'
+
+//permission  add confirm
+
+// เพิ่มเส้นขีดใต้ tr
+// ปรับรูปให้อยู่กลาง
+// เพิ่ม icon option หลังระดับสิทธิ
+// ทำให้เชื่อมกัย Data
+// search engine ต้องใช้ได้
+// ปรับสมดุล
+// เพิ่ม Navbar
+// ใส่ background Pic
+// ใน dataมีกี่รายชื่อต้องใส่ทั้งหมด
+// มีหลายหน้าได้หรือสไลเรื่อนลงเก็ได้
+// ทำ pop up option
+
+//Stats part
+//ปรับสมดุล text ใน Box
+//make graph box
+//make graph
+//make line graph
+//ดึงเลขมาจาก database คน/เอกสาร
+
+function App() {
+
+  return (
+    <div className="App">
+      <p>Hi Welcome</p>
+    </div>
+  )
+}
+
+export default App
