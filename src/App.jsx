659324402a8b6a68c4db68efa5740717d4f9c38e
tatsunori.kat@spumail.net <norijang.kato@gmail.com>
2024-11-17 17:20:23 +0700

    แก้ route ให้สามารถใช้เชื่อมคนอื่นได้ง่าย

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,38 @@
-import { HashRouter,Routes, Route } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import SearchBar from './pages/Searchbar/Searchbar';
 
 
 import Layout from './Layout/Layout';
+import Permission from './pages/Permission/Permission';
+import Stats from './pages/Stats/Stats';
+import Navbar from './Layout/Navber/Navber';
 
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.min.css";
-
 import './App.css'
 
 
 
 function App() {
-
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+    },
+    { path: "search",
+      element: <Navbar />,
+    },{
+      path: "permission",
+      element: <Permission />,
+    },
+    {
+      path: "stats",
+      element: <Stats />,
+    }
+  ]);
   return (
-    <>
-      <div className="App">
-        <HashRouter>
-          <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route path='/' element={<SearchBar />} />
-          </Route>
-        </Routes>
-        </HashRouter>
-      </div>
-    </>
+   <RouterProvider router={router}/>
   )
 }
 
