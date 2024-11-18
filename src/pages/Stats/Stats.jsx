370d9f94cafee15c78e66199b31877f9c258997b
tatsunori.kat@spumail.net <norijang.kato@gmail.com>
2024-11-18 20:21:08 +0700

    Stats

--- src/pages/Stats/Stats.jsx
+++ src/pages/Stats/Stats.jsx
@@ -7,68 +7,92 @@ import sourceData from '../../data/sourceData.json'
 
 function Stats() {
   return (
-    <div>
+    <div className='stats'>
+      
+      
       <div className='statsContainer'>
 
-        <div className='boxStats'>
-          <span className='textStatsTopic'>จำนวนเอกสาร</span>
-          <span className='textStatsNum'>500</span>
+        <div className='statsContainer1'>
+          <div className='boxStats'>
+            <span className='textStatsTopic'>จำนวนเอกสาร</span>
+            <span className='textStatsNum'>500</span>
+          </div>
+          <div className='boxStats'>
+            <span className='textStatsTopic'>จำนวนเอกสาร</span>
+            <span className='textStatsNum'>500</span>
+          </div>
+          <div className='boxStats'>
+            <span className='textStatsTopic'>จำนวนเอกสาร</span>
+            <span className='textStatsNum'>500</span>
+          </div>
+          <div className='boxStats'>
+            <span className='textStatsTopic'>จำนวนเอกสาร</span>
+            <span className='textStatsNum'>500</span>
+          </div>
+          <div className='boxStats'>
+            <span className='textStatsTopic'>จำนวนเอกสาร</span>
+            <span className='textStatsNum'>500</span>
+          </div>
+          <div className='boxStats'>
+            <span className='textStatsTopic'>จำนวนเอกสาร</span>
+            <span className='textStatsNum'>500</span>
+          </div>
+          <div className='boxStats'>
+            <span className='textStatsTopic'>จำนวนเอกสาร</span>
+            <span className='textStatsNum'>500</span>
+          </div>
+          <div className='boxStats'>
+            <span className='textStatsTopic'>จำนวนเอกสาร</span>
+            <span className='textStatsNum'>500</span>
+          </div>
         </div>
 
 
-        <div className='boxStats'>
-          <span className='textStatsTopic'>จำนวนเอกสาร</span>
-          <span className='textStatsNum'>500</span>
+
+
+
+
+
+
+
+
+        <div>
+
+          <div className='BarChartContainer'>
+            <Bar
+              data={{
+                labels: sourceData.map((data) => data.label),
+                datasets: [
+                  {
+                    label: 'จำนวนเข้าชม',
+                    data: sourceData.map((data) => data.value),
+                    backgroundColor: [
+                      'red', 'green', 'blue'
+                    ]
+                  }]
+              }} />
+
+          </div>
         </div>
-
-        <div className='boxStats'>
-          <span className='textStatsTopic'>จำนวนเอกสาร</span>
-          <span className='textStatsNum'>500</span>
-        </div>
-
-        <div className='boxStats'>
-          <span className='textStatsTopic'>จำนวนเอกสาร</span>
-          <span className='textStatsNum'>500</span>
-        </div>
-
-
       </div>
-      <div className='BarChartContainer'>
-        <Bar
-          data={{
-            labels: sourceData.map((data) => data.label),
-            datasets: [
-              {
-                label: 'จำนวนเข้าชม',
-                data: sourceData.map((data) => data.value),
-                backgroundColor: [
-                  'red', 'green', 'blue'
-                ]
-              }
-            ]
-          }
+      <div className='DoughnutChartBg'>
 
-          }
-        />
-      </div>
-
-      <div className='DoughnutChartContainer'>
-        <Doughnut
-          data={{
-            labels: sourceData.map((data) => data.label),
-            datasets: [
-              {
-                label: 'จำนวนเข้าชม',
-                data: sourceData.map((data) => data.value),
-                backgroundColor: [
-                  'red', 'green', 'blue'
-                ]
-              }
-            ]
-          }
-
-          }
-        />
+        <div className='DoughnutChartContainer'>
+          <Doughnut
+            data={{
+              labels: sourceData.map((data) => data.label),
+              datasets: [
+                {
+                  label: 'จำนวนเข้าชม',
+                  data: sourceData.map((data) => data.value),
+                  backgroundColor: [
+                    'red', 'green', 'blue'
+                  ]
+                }
+              ]
+            }}
+          />
+        </div>
       </div>
 
     </div>
