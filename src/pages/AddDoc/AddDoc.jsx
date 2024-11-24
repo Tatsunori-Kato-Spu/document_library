db271df8de19e8ce05ee9eb7dfb48778253a8897
tatsunori.kat@spumail.net <norijang.kato@gmail.com>
2024-11-24 16:27:02 +0700

    แก้แสดงเอกสารหลัง app

--- src/pages/AddDoc/AddDoc.jsx
+++ src/pages/AddDoc/AddDoc.jsx
@@ -34,7 +34,7 @@ function AddDoc() {
         const newDoc = {
             หมายเลข: formData.docNumber,
             ชื่อเอกสาร: formData.docName,
-            เรื่อง: `เรื่องของเอกสาร ${formData.docName}`,
+            เรื่อง: `${formData.docName}`,
             หน่วยงาน: formData.department,
             วันที่: formattedDate, // ใช้วันที่ที่ฟอร์แมตแล้ว
             เวลา: new Date().toLocaleTimeString("th-TH", { hour: '2-digit', minute: '2-digit', hour12: false }), // รูปแบบ 24 ชั่วโมง ไม่มีวินาที
